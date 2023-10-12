74598d55d0fe251ed5437d8a9245d003a8ffd9cd
JoaoPaulo-creator <joaosarto@gmail.com>
2023-10-12 00:34:55 -0300

    refactor: removed all unnecessary files

--- src/app.container.ts
+++ src/app.container.ts
@@ -1,7 +1,5 @@
 import { Container } from 'inversify';
 import { AppContainer } from '@expressots/core';
-import { AppModule } from '@useCases/app/app.module';
-import { UserModule } from '@useCases/user/user.module';
 import { EmployeeModule } from '@useCases/employee/employee.module';
 import { EmployerModule } from '@useCases/employer/employer.module';
 import { PunchTheCardModule } from '@useCases/punch-the-card/punch-the-card.module';
@@ -10,8 +8,6 @@ const appContainer = new AppContainer();
 
 export const container: Container = appContainer.create([
   // Add your modules here
-  AppModule,
-  UserModule,
   EmployeeModule,
   EmployerModule,
   PunchTheCardModule,
